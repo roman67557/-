c3a0b140b2d55055348f94d56a41e1680b55291c
Roman <bazhan.roma@icloud.com>
2022-04-14 15:29:45 +0300

    Update AppDelegate.swift

--- ??/AppDelegate.swift
+++ ??/AppDelegate.swift
@@ -10,27 +10,31 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //MARK: Custom Font
+//        UIFont.familyNames.forEach({ name in
+//
+//            for font_name in UIFont.fontNames(forFamilyName: name) {
+//                print("\n\(font_name)")
+//            }
+//
+//        })
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        
+        let tabBarController = UITabBarController()
+        let assemblyBuilder = AssemblyModelBuilder()
+        let router = Router(navigationController: tabBarController, assemblyBuilder: assemblyBuilder)
+        router.initialViewController()
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
 
 }
 
