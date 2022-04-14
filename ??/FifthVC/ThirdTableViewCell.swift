270efca50a116166dc12fb6d137eee64506013bd
Roman <bazhan.roma@icloud.com>
2022-04-14 15:31:11 +0300

    Create ThirdTableViewCell.swift

---
+++ ??/FifthVC/ThirdTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdTableViewCell.swift
+//  wq
+//
+//  Created by Роман on 31.12.2021.
+//
+
+import UIKit
+
+class ThirdTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
