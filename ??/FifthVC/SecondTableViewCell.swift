d46de0e2d963c800b6804e472bae7294142bf356
Roman <bazhan.roma@icloud.com>
2022-04-14 15:31:09 +0300

    Create SecondTableViewCell.swift

---
+++ ??/FifthVC/SecondTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SecondTableViewCell.swift
+//  wq
+//
+//  Created by Роман on 30.12.2021.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
